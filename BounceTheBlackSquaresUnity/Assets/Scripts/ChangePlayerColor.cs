5d5e4fc4436454f673f5963d68e8d98119bfbe12
Gilles Lengy <gilles.lengy@gmail.com>
2016-06-20 21:40:47 +0200

    The player doesn't go red when hitting a black square

--- BounceTheBlackSquaresUnity/Assets/Scripts/ChangePlayerColor.cs
+++ BounceTheBlackSquaresUnity/Assets/Scripts/ChangePlayerColor.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class ChangePlayerColor : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collission)
     {
-        GetComponent<SpriteRenderer>().color = Color.red;
+        if (collission.gameObject.tag != "BouncingSquare")
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
     }
     void OnCollisionExit2D(Collision2D collission)
     {
