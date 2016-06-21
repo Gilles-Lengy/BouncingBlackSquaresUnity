65cd7e61afb2f57a66b6121c72872b777a0df286
Gilles Lengy <gilles.lengy@gmail.com>
2016-06-21 22:01:55 +0200

    Blacks squares are now spawned

---
+++ BounceTheBlackSquaresUnity/Assets/Scripts/SpawnBlackSquares.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBlackSquares : MonoBehaviour {
+    public GameObject spriteToDuplicate;
+
+    // Use this for initialization
+    void Start () {
+        Vector3 currentPosition = spriteToDuplicate.transform.position;
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject tmpObj = GameObject.Instantiate(spriteToDuplicate, currentPosition, Quaternion.identity) as GameObject;
+            currentPosition += new Vector3(1f, 0f, 0f);
+        }
+    }
+
+	
+
+}
