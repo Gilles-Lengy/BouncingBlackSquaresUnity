a80f2888cf0f3f3e5a98f880c3cadb7507a54ab4
Gilles Lengy <gilles.lengy@gmail.com>
2016-06-21 22:30:03 +0200

    The player as now a RandomVelocity script that have fixed velocity value... This script nee to become a SetVelocity according to the 'bouton' that wil be added on the sides of the game... Well, I know what I mean ! Hope I'll remember it later on...

---
+++ BounceTheBlackSquaresUnity/Assets/Scripts/RandomVelocity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomVelocity : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+            rb.velocity = new Vector2(15,15);
+
+    }
+}
