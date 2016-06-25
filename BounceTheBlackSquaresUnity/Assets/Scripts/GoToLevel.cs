07373a1543bc1bcad1f7819675e813df862a4d3b
Gilles Lengy <gilles.lengy@gmail.com>
2016-06-25 19:59:13 +0200

    Better agnecement of the little black squares at the beginning of the game and a..... Restart button !

---
+++ BounceTheBlackSquaresUnity/Assets/Scripts/GoToLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel : MonoBehaviour
+{
+    public void OnClickGoToLevel(string level)
+    {
+        Time.timeScale = 1F;// http://docs.unity3d.com/ScriptReference/Time-timeScale.html
+        SceneManager.LoadScene(level);
+        Debug.Log("Go to level" + level);
+
+    }
+}
