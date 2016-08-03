69e9b6f2ce14deda0633101f8203e7aa75d3c9b3
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-03 09:40:25 +0200

    LinearSpritesSpawner2D script added

---
+++ BouncingBlackSquaresUnity/Assets/Scripts/LinearSpritesSpawner2D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinearSpritesSpawner2D : MonoBehaviour {
+
+    public GameObject spriteToDuplicate;
+    public float startPositionX;
+    public float StartPositionY;
+    public float gapAnchorX;
+    public float gapAnchorY;
+    public int iteration;
+
+    // Use this for spawning
+    void Start()
+    {
+        // The position of the first generated sprite
+        Vector2 spritePosition = new Vector2(startPositionX, StartPositionY);
+        // Spawning the sprites
+        for (int i = 0; i < iteration; i++)
+        {
+            GameObject.Instantiate(spriteToDuplicate, spritePosition, Quaternion.identity);
+            spritePosition += new Vector2(gapAnchorX, gapAnchorY);
+        }
+        // Destroying the original sprite
+        Destroy(spriteToDuplicate);
+    }
+}
